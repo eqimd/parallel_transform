bb38121b043d513276846275cfe36635b44cc34c
Daniil Lyubaev <lyubaevda@gmail.com>
2022-05-04 10:25:10 +0300

    Add range scheduler with futex

---
+++ src/range_scheduler/range_scheduler.cpp
@@ -0,0 +1,46 @@
+#include "range_scheduler.h"
+#include <stddef.h>
+#include "futex/futex.h"
+#include <sys/mman.h>
+#include <algorithm>
+
+
+range_scheduler::range_scheduler(size_t orig_sz, size_t subrange_sz) {
+    _orig_sz = orig_sz;
+    _subrange_sz = subrange_sz;
+    _cur_pos = 0;
+
+    sched_futex = reinterpret_cast<int32_t*>(
+        mmap(
+            NULL,
+            sizeof(int32_t),
+            PROT_READ | PROT_WRITE,
+            MAP_SHARED | MAP_ANONYMOUS,
+            -1,
+            0
+        )
+    );
+
+    *sched_futex = 1;   /* Make futex available */
+}
+
+range_scheduler::~range_scheduler() {
+    munmap(sched_futex, sizeof(int32_t));
+}
+
+std::pair<position, length> range_scheduler::get_subrange() {
+    fwait(sched_futex);
+
+    if (_cur_pos >= _orig_sz) {
+        fpost(sched_futex);
+        return {-1, -1};
+    }
+
+    size_t len = std::min(_subrange_sz, _orig_sz - _cur_pos);
+    size_t ret_pos = _cur_pos;
+    _cur_pos += len;
+
+    fpost(sched_futex);
+
+    return {ret_pos, len};
+}
---
+++ src/range_scheduler/range_scheduler.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+#include <utility>
+
+using position = size_t;
+using length = size_t;
+
+
+class range_scheduler {
+public:
+    range_scheduler(size_t orig_sz, size_t subrange_sz);
+    ~range_scheduler();
+    std::pair<position, length> get_subrange();
+private:
+    size_t _orig_sz;
+    size_t _cur_pos;
+    size_t _subrange_sz;
+
+    int32_t* sched_futex;
+};
