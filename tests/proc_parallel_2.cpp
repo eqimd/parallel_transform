bfcf47a020bc3c3aa6095bc0d466aab142ef016d
Daniil Lyubaev <lyubaevda@gmail.com>
2022-05-05 20:01:23 +0300

    Add tests for parallelism

---
+++ tests/proc_parallel_2.cpp
@@ -0,0 +1,21 @@
+#include "transform/transform.h"
+#include <vector>
+#include <iostream>
+#include <unistd.h>
+#include <chrono>
+
+
+int main() {
+    std::vector<int> v(20, 1);
+
+    auto begin = std::chrono::steady_clock::now();
+    auto v_trans = TransformWithProcesses(v, [](int x) {sleep(1); return x + 1;}, 5, 4);
+    auto end = std::chrono::steady_clock::now();
+    auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+
+    if (diff < 7) {
+        return 0;
+    }
+
+    return 1;
+}
